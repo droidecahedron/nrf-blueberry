03a857d6f0d45af5198a6b81cb318043e12c07b9
Nguyen <johnny.nguyen@nordicsemi.no>
2023-08-17 11:31:42 -0700

    new main for calling pdm transfers then classification

--- src/main.cpp
+++ src/main.cpp
@@ -121,6 +121,89 @@ static int do_pdm_transfer(const struct device *dmic_dev,
 }
 
 
+void main()
+{
+	int ret;
+	ret = ei_wrapper_init(result_ready_cb);
+
+	if(ret){
+		LOG_ERR("EI wrapper failed to init: %i", ret);
+	}
+    k_sleep(K_SECONDS(15));
+	
+	const struct device *dmic_dev = DEVICE_DT_GET(DT_NODELABEL(dmic_dev));
+	LOG_INF("DMIC module");
+	if (!device_is_ready(dmic_dev)) {
+		LOG_ERR("%s is not ready", dmic_dev->name);
+		return;
+	}
+
+	/* Configuration of the microphone. */
+	struct pcm_stream_cfg stream = {
+		.pcm_width = SAMPLE_BIT_WIDTH,
+		.mem_slab  = &mem_slab,
+	};
+	struct dmic_cfg cfg = {
+		.io = {
+			/* These fields can be used to limit the PDM clock
+			 * configurations that the driver is allowed to use
+			 * to those supported by the microphone.
+			 */
+			.min_pdm_clk_freq = 1100000,
+			.max_pdm_clk_freq = 3500000,
+			.min_pdm_clk_dc   = 40,
+			.max_pdm_clk_dc   = 60,
+		},
+		.streams = &stream,
+		.channel = {
+			.req_num_streams = 1,
+		},
+	};
+
+	cfg.channel.req_num_chan = 1;
+	cfg.channel.req_chan_map_lo =
+		dmic_build_channel_map(0, 0, PDM_CHAN_LEFT);
+	cfg.streams[0].pcm_rate = MAX_SAMPLE_RATE;
+	cfg.streams[0].block_size =
+		BLOCK_SIZE(cfg.streams[0].pcm_rate, cfg.channel.req_num_chan);
+
+
+	cfg.channel.req_num_chan = 2;
+	cfg.channel.req_chan_map_lo =
+		dmic_build_channel_map(0, 0, PDM_CHAN_LEFT) |
+		dmic_build_channel_map(1, 0, PDM_CHAN_RIGHT);
+	cfg.streams[0].pcm_rate = MAX_SAMPLE_RATE;
+	cfg.streams[0].block_size =
+		BLOCK_SIZE(cfg.streams[0].pcm_rate, cfg.channel.req_num_chan);
+
+	ret = dmic_configure(dmic_dev, &cfg);
+	if (ret < 0) {
+		return;
+	}
+
+
+	while(true){
+ 
+
+			ret = do_pdm_transfer(dmic_dev, &cfg, 2 * BLOCK_COUNT); // ei classification happens in here
+			if (ret < 0) {
+				return;
+			k_sleep(K_SECONDS(15));
+		 }
+			k_sleep(K_SECONDS(1));
+	}
+}
+
+
+
+
+
+
+
+
+
+/* Old static classification main*/
+/*
 int main() {
     // This is needed so that output of printf is output immediately without buffering
     setvbuf(stdout, NULL, _IONBF, 0);
@@ -178,3 +261,4 @@ int main() {
         k_msleep(12000);
     }
 }
+*/
